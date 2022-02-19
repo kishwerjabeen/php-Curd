ab1841bd05455dbdcf590add415c1942b547f5b1
Kishwar  Jabeen <kishwerjabeen90@gmail.com>
2022-02-19 15:21:23 +0500

    database conected

---
+++ conn.php
@@ -0,0 +1,20 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "curdphp";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+
+/* if ($conn) {
+    echo "connection hogya bacho";
+} else {
+    // echo "connection Mahe howa bacho";
+    die("connection nahe howa bacho" . mysqli_connect_error());
+} */
+
+if ($conn->connect_error) {
+    die("connection nahe howa bacho: " . $conn->connect_error);
+}
+echo "connection hogya bacho";
