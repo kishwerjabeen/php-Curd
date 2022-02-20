e72ad2447a6ee0b348d7c7d30be6055debd0fd50
Kishwar  Jabeen <kishwerjabeen90@gmail.com>
2022-02-20 18:26:52 +0500

    data insert in sql

--- conn.php
+++ conn.php
@@ -5,17 +5,18 @@ $password = "";
 $db = "curdphp";
 
 // Create connection
-// $conn = new mysqli_connect($servername, $username, $password, $db);
-$conn = new mysqli($servername, $username, $password, $db);
+$conn = mysqli_connect($servername, $username, $password, $db);
+// $conn = new mysqli($servername, $username, $password, $db);
 
-/* if ($conn) {
-    echo "connection hogya bacho";
+if ($conn) {
+    // echo "connection hogya bacho";
+?>
+    <script>
+        alert('connection hogya bacho');
+    </script>
+<?php
 } else {
     // echo "connection Mahe howa bacho";
     die("connection nahe howa bacho" . mysqli_connect_error());
-} */
-
-if ($conn->connect_error) {
-    die("connection nahe howa bacho: " . $conn->connect_error);
 }
-echo "connection hogya bacho";
+?>
--- index.php
+++ index.php
@@ -10,27 +10,81 @@
 
     <div class="container mt-3">
         <h2>Stacked form</h2>
-        <form action="/action_page.php">
+        <form action="index.php" method="POST">
+
+            <div class="mb-3">
+                <label for="name">Name:</label>
+                <input type="text" class="form-control" id="name" placeholder="Enter Name" name="user" value="" required />
+            </div>
+
+            <div class="mb-3">
+                <label for="deg">Degree:</label>
+                <input type="text" class="form-control" id="deg" placeholder="Enter Degree" name="deg" value="" required />
+            </div>
+
+            <div class="mb-3">
+                <label for="numb">Mobile Number:</label>
+                <input type="tel" class="form-control" id="numb" placeholder="mobile Number" name="mobile" value="" required />
+            </div>
+
             <div class="mb-3 mt-3">
                 <label for="email">Email:</label>
-                <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                <input type="email" class="form-control" id="email" placeholder="Any refference" name="email" value="" required />
             </div>
+
             <div class="mb-3">
-                <label for="pwd">Password:</label>
-                <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+                <label for="ref">Refference:</label>
+                <input type="text" class="form-control" id="ref" placeholder="Any Refference" name="ref" value="" required />
             </div>
-            <div class="form-check mb-3">
-                <label class="form-check-label">
-                    <input class="form-check-input" type="checkbox" name="remember"> Remember me
-                </label>
+
+            <div class="mb-3">
+                <label for="jobpro">Job Profile:</label>
+                <input type="text" class="form-control" id="jobpro" placeholder="Webdeveloper Post" name="jobpro" value="Web developer Post" required />
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+
+
+
+            <button type="submit" name="submit" class="btn btn-primary">Submit</button>
         </form>
+
+        <br>
+        <a href="display.php" class="btn btn-success btn-block">Link Button</a>
     </div>
+
 </body>
 
 </html>
 
 <?php
 include 'conn.php';
+if (isset($_POST['submit'])) {
+    $name = $_POST['user'];
+    $quli = $_POST['deg'];
+    $number = $_POST['mobile'];
+    $email = $_POST['email'];
+    $ref = $_POST['ref'];
+    $jobs = $_POST['jobpro'];
+
+    $insertquery = "INSERT INTO jobreg (firname, degree, mobile, email, refer, jobpost) VALUES ('$name','$quli','$number','$email','$ref','$jobs')";
+
+
+
+    $res = mysqli_query($conn, $insertquery);
+
+
+
+    if ($res) {
+?>
+        <script>
+            alert("Data Insert Hogya ha Yaaaa");
+        </script>
+    <?php
+    } else {
+    ?>
+        <script>
+            alert("Data Insert Hogya ha Yaaaa");
+        </script>
+<?php
+    }
+}
 ?>
