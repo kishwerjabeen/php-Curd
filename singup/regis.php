f2386b588fed958f608ffb01b1c7853ff3820ea0
Kishwar  Jabeen <kishwerjabeen90@gmail.com>
2022-02-21 20:11:01 +0500

    signup

---
+++ singup/regis.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <?php include '../links.php' ?>
+    <title>Registration</title>
+</head>
+
+<body>
+    <?php
+    include '../conn.php';
+    if (isset($_POST['submit'])) {
+
+        $username = mysqli_real_escape_string($conn, $_POST['username']);
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $mobile = mysqli_real_escape_string($conn, $_POST['mobile']);
+        $password = mysqli_real_escape_string($conn, $_POST['password']);
+        $cpassword = mysqli_real_escape_string($conn, $_POST['cpassword']);
+
+        $pass = password_hash($password, PASSWORD_BCRYPT);
+        $cpass = password_hash($cpassword, PASSWORD_BCRYPT);
+
+        $emailQuery = "SELECT * FROM reg WHERE email = '$email'";
+        $regQuery = mysqli_query($conn, $emailQuery);
+        $emailCount = mysqli_num_rows($regQuery);
+
+        if ($emailCount > 0) {
+            echo "Email already exists";
+        } else {
+            if ($pass == $cpass) {
+                $insertQury = "INSERT INTO reg( username, email, mobile, password, cpassword) VALUES ('$username','$email','$mobile','$pass','$cpass')";
+                $iqury = mysqli_query($conn, $insertQury);
+
+                if ($iqury) {
+    ?>
+                    <script>
+                        alert('Inserted hogya bacho');
+                    </script>
+                <?php
+                } else {
+                ?>
+                    <script>
+                        alert('Inserted Mahi howa bacho');
+                    </script>
+    <?php
+                }
+            } else {
+                echo "Password not Matching";
+            }
+        }
+    }
+
+
+
+    ?>
+
+
+
+    <section class="vh-100" style="background-color: #eee;">
+        <div class="container h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-lg-12 col-xl-11">
+                    <div class="card text-black" style="border-radius: 25px;">
+                        <div class="card-body p-md-5">
+                            <div class="row justify-content-center">
+                                <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+
+                                    <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+                                    <div class="btn-group-vertical" role="group">
+                                        <button type="button" class="btn btn-labeled btn-danger " style="align-items: center;">
+                                            <span class="btn-label"><i class="fa fa-camera"></i></span> Login Google</button>
+                                        <br>
+                                        <button type="button" class="btn btn-labeled btn-primary" style="align-items: center;">
+                                            <span class="btn-label"><i class="fa fa-camera"></i></span> Login Facebook</button>
+
+                                    </div>
+                                    <br>
+                                    <br>
+                                    <form class="mx-1 mx-md-4" action="" method="POST">
+
+                                        <div class="d-flex flex-row align-items-center mb-4">
+                                            <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                                            <div class="form-outline flex-fill mb-0">
+                                                <input type="text" id="form3Example1c" class="form-control" name="username" value="" required />
+                                                <label class="form-label" for="form3Example1c">Your Name</label>
+                                            </div>
+                                        </div>
+
+                                        <div class="d-flex flex-row align-items-center mb-4">
+                                            <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                                            <div class="form-outline flex-fill mb-0">
+                                                <input type="email" id="form3Example3c" class="form-control" name="email" value="" required />
+                                                <label class="form-label" for="form3Example3c">Your Email</label>
+                                            </div>
+                                        </div>
+                                        <div class="d-flex flex-row align-items-center mb-4">
+                                            <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                                            <div class="form-outline flex-fill mb-0">
+                                                <input type="tel" id="form3Example7c" class="form-control" name="mobile" value="" required />
+                                                <label class="form-label" for="form3Example7c">Your Mobile</label>
+                                            </div>
+                                        </div>
+                                        <div class="d-flex flex-row align-items-center mb-4">
+                                            <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                                            <div class="form-outline flex-fill mb-0">
+                                                <input type="password" id="form3Example4c" class="form-control" name="password" value="" required />
+                                                <label class="form-label" for="form3Example4c">Password</label>
+                                            </div>
+                                        </div>
+
+                                        <div class="d-flex flex-row align-items-center mb-4">
+                                            <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                                            <div class="form-outline flex-fill mb-0">
+                                                <input type="password" id="form3Example4cd" class="form-control" name="cpassword" value="" required />
+                                                <label class=" form-label" for="form3Example4cd">Repeat your password</label>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-check d-flex justify-content-center mb-5">
+                                            <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
+                                            <label class="form-check-label" for="form2Example3">
+                                                I agree all statements in <a href="#!">Terms of service</a>
+                                            </label>
+                                        </div>
+
+                                        <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                                            <button type="submit" name="submit" class="btn btn-primary btn-lg">Register</button>
+                                        </div>
+
+                                    </form>
+
+                                </div>
+                                <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp" class="img-fluid" alt="Sample image">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+</body>
+
+</html>
