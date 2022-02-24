fd12176efc3fa82f96cdca96c06c140d37e3d5c6
Kishwar  Jabeen <kishwerjabeen90@gmail.com>
2022-02-24 14:55:28 +0500

    complate signup

--- singup/regis.php
+++ singup/regis.php
@@ -146,9 +146,11 @@ session_start();
                                         </div>
 
                                     </form>
-
+                                    <p>Have an Account <a href="#">Login</a></p>
                                 </div>
+
                                 <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
 
                                     <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp" class="img-fluid" alt="Sample image">
 
