d260b4c8574e2dc2981bac57e699980d7d5d7fc8
Kishwar  Jabeen <kishwerjabeen90@gmail.com>
2022-03-29 15:51:39 +0500

    Session read conditions

--- session/sessionread.php
+++ session/sessionread.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
 
-echo "mY name is " . $_SESSION['ussername'] . "and my age is ";
-echo $_SESSION['age'];
+if (isset($_SESSION['ussername'])) {
+    echo "mY name is " . $_SESSION['ussername'] . "and my age is ";
+    echo $_SESSION['age'];
+} else {
+    echo "no usser is set";
+}
